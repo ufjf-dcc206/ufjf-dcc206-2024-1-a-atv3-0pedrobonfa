a0538b57a591fa0f7d87b915f2cb10358b2df0c1
Pedro Bonfá <pedro.bonfa@estudante.ufjf.br>
2024-09-24 21:51:03 -0300

    faz interface pokemon e consegue o retorno de nome e id

--- ufjf-dcc206-2024-1-a-atv3-0pedrobonfa/src/main.ts
+++ ufjf-dcc206-2024-1-a-atv3-0pedrobonfa/src/main.ts
@@ -1,1 +1,32 @@
 import './style.css'
+import './style.css'
+
+
+
+interface Pokemon
+{
+  name: string;
+  id: number;
+  url: string;
+}
+
+
+
+
+async function fetchPokemonDetails() {
+  
+  const response = await fetch("https://pokeapi.co/api/v2/pokemon/1");
+  if (!response.ok) {
+    throw new Error(`Erro na rede: ${response.statusText}`);
+  }
+
+  const data: Pokemon = await response.json();
+
+  console.log(`Nome: ${data.name}`);
+  
+  console.log(`ID: ${data.id}`);
+
+  
+}
+
+fetchPokemonDetails();
