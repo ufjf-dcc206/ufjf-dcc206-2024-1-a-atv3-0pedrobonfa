529adb95f10cabb0ea135ab76407d278ecc25dc6
Pedro Bonfá <pedro.bonfa@estudante.ufjf.br>
2024-09-24 21:53:22 -0300

    retorna tipo dos pokemon e acessa pokemon aleatório

--- ufjf-dcc206-2024-1-a-atv3-0pedrobonfa/src/main.ts
+++ ufjf-dcc206-2024-1-a-atv3-0pedrobonfa/src/main.ts
@@ -1,21 +1,28 @@
 import './style.css'
-import './style.css'
 
-
+interface PokeType{
+  type:{
+    name:string;
+  }
+}
 
 interface Pokemon
 {
   name: string;
   id: number;
   url: string;
+  types: PokeType[];
 }
 
 
+function getRandomNum(min: number, max: number): number {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 
 async function fetchPokemonDetails() {
   
-  const response = await fetch("https://pokeapi.co/api/v2/pokemon/1");
+  const response = await fetch("https://pokeapi.co/api/v2/pokemon/"+getRandomNum(1,1025));
   if (!response.ok) {
     throw new Error(`Erro na rede: ${response.statusText}`);
   }
@@ -25,7 +32,12 @@ async function fetchPokemonDetails() {
   console.log(`Nome: ${data.name}`);
   
   console.log(`ID: ${data.id}`);
-
+  
+  for(let i=0; i<data.types.length; i++)
+  {
+    console.log(`Type: ${data.types[i].type.name}`)
+  }
+    
   
 }
 
